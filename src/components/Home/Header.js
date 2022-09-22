f96f3194df5e0d41ad52d129f7c117753c3b88f5
DizzlyD <david4henery00@hotmail.com>
2022-09-22 14:53:26 +0100

    products & header tab

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -1,16 +1,22 @@
 import React from "react";
 import headerImage from "../../assets/home/mobile/image-header.jpg"
+import headerImgTab from "../../assets/home/tablet/image-header.jpg";
 
 const Header = () => {
   return (
     <header className="header">
+      <picture className="header__picture">
+        <source srcSet={headerImgTab} media="(min-width:34.68em)"/>
       <img
         className="header__image"
-        src={headerImage}
+        srcSet={headerImage}
         alt="headerphones-header"
       />
+      </picture>
       <p className="header__tag overline">NEW PRODUCT</p>
-      <h1 className="header__title h1-style">XX99 Mark II Headphones</h1>
+      <h1 className="header__title h1-style">
+        <span>XX99 Mark II </span> 
+        Headphones</h1>
       <p className="header__desc body-text">
         Experience natural, lifelike audio and exceptional build quality made
         for the passionate music enthusiast.
